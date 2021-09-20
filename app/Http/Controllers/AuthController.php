e783bc7654997639c67ba9c320300ee03a4c80e3
vishnu tej k <vtej@v2soft.com>
2021-09-20 20:57:42 +0530

    JWT Authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $email = $request->email;
+        $password = $request->password;
+
+        // Check if field is not empty
+        if (empty($email) or empty($password)) {
+            return response()->json(['status' => 'error', 'message' => 'You must fill all fields']);
+        }
+        $credentials = request(['email', 'password']);
+
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        $name = $request->name;
+        $email = $request->email;
+        $password = $request->password;
+
+        if (empty($name) or empty($email) or empty($password)) {
+            return response()->json(['status' => 'error', 'message' => 'You must fill all the fields']);
+        }
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return response()->json(['status' => 'error', 'message' => 'You must enter a valid email']);
+        }
+
+        if (strlen($password) < 6) {
+            return response()->json(['status' => 'error', 'message' => 'Password should be min 6 character']);
+        }
+
+        if (User::where('email', '=', $email)->exists()) {
+            return response()->json(['status' => 'error', 'message' => 'User already exists with this email']);
+        }
+
+        try {
+            $user = new User();
+            $user->name = $name;
+            $user->email = $email;
+            $user->password = app('hash')->make($password);
+
+            if ($user->save()) {
+                // Will call login method
+                return $this->login($request);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'error', 'message' => $e->getMessage()]);
+        }
+    }
+}
